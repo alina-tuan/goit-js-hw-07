22ea3917966f63e1132a7d0aa050d518ec8539a8
Alina <alina.bilchich@gmail.com>
2022-10-20 13:55:06 +0300

    Task 1 is done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryItemsMarkup = createGalleryItemMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+
+
+function createGalleryItemMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div >
+`;
+    })
+      .join('');
+};
+
+galleryContainer.addEventListener('click', onGalleryImgClick);
+function onGalleryImgClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+    return
+    };
+    const modalwindow = basicLightbox.create(`
+    <img src = "${event.target.dataset.source}"> width = "800" height = "600"
+    `);
+    modalwindow.show();
+  console.log(event.target);
+};
+
+
+// console.log(galleryItems);
